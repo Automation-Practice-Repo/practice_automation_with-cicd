1d7e286dccdb059e11acf9c8d1ebb6f805f2f202
hari <hari@gm.com>
2026-03-10 10:13:20 +0530

    LEVEL 1 — Git & Project Setup (Beginner) ::hari

---
+++ ci_cd_game/tests/test_login.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+def test_open_page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        page.goto("https://the-internet.herokuapp.com/login")
+
+        assert "The Internet" in page.title()
+
+        browser.close()
