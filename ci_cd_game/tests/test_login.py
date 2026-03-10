5d4cc2ec6ff4a75801501a1de5a4f6566bcb5830
hari <hari@gm.com>
2026-03-10 10:19:26 +0530

    LEVEL 2 — Page Object Model (Intermediate)::hari

--- ci_cd_game/tests/test_login.py
+++ ci_cd_game/tests/test_login.py
@@ -10,3 +10,20 @@ def test_open_page():
         assert "The Internet" in page.title()
 
         browser.close()
+
+from playwright.sync_api import sync_playwright
+from ci_cd_game.pages.login_page import LoginPage
+
+def test_login():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        page.goto("https://the-internet.herokuapp.com/login")
+
+        login = LoginPage(page)
+        login.login("tomsmith", "SuperSecretPassword!")
+
+        assert "Secure Area" in page.content()
+
+        browser.close()
